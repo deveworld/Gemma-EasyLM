3650abc4065684f068438ca60291470be66949c2
World <simworld415@gmail.com>
2024-04-16 23:58:51 +0900

    Update convert_easylm_stream_to_hf_safetensors.py

--- convert_easylm_stream_to_hf_safetensors.py
+++ convert_easylm_stream_to_hf_safetensors.py
@@ -10,7 +10,7 @@ from transformers import FlaxGemmaForCausalLM
 
 _, param = StreamingCheckpointer.load_trainstate_checkpoint(load_from=f'trainstate_params::{ckpt_path}')
 
-gemma_config = GemmaConfig.from_pretrained("google/gemma-7b")
+gemma_config = GemmaConfig.from_pretrained("google/gemma-2b")
 
 # EasyLM Gemma
 # model = FlaxGemmaForCausalLMModule(
@@ -32,19 +32,19 @@ auto_model = FlaxGemmaForCausalLM(config=gemma_config) # HF Gemma
 auto_model.params = params['params']
 
 # 단일 파일로 로드해야 함
-auto_model.save_pretrained('./flax-gemma-ko-8b', max_shard_size='999GB')
+auto_model.save_pretrained('./flax-gemma-ko-2b', max_shard_size='999GB')
 
 # HF Flax --> HF SafeTensors
 import torch
 from transformers import GemmaForCausalLM
 
-hf_model = GemmaForCausalLM.from_pretrained('./flax-gemma-ko-8b/', 
+hf_model = GemmaForCausalLM.from_pretrained('./flax-gemma-ko-2b/', 
                                             from_flax=True)
 hf_model.to(torch.bfloat16)
 hf_model.config.torch_dtype = torch.bfloat16
-hf_model.save_pretrained('./gemma-ko-7b-dev', max_shard_size='4GB')
+hf_model.save_pretrained('./gemma-ko-2b-dev', max_shard_size='4GB')
 
 from transformers import AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b")
-tokenizer.save_pretrained('./gemma-ko-7b-dev')
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b")
+tokenizer.save_pretrained('./gemma-ko-2b-dev')
