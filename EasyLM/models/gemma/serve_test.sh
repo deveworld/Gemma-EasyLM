9f88968a26c11a82197d59703392e542c03666ac
World <simworld415@gmail.com>
2024-04-17 00:03:49 +0900

    Update serve_test.sh

--- EasyLM/models/gemma/serve_test.sh
+++ EasyLM/models/gemma/serve_test.sh
@@ -1,8 +1,8 @@
 export PYTHONPATH=$(realpath ../../../):$PYTHONPATH
 export CUDA_VISIBLE_DEVICES='1'
 python gemma_serve.py \
---load_checkpoint="flax_params::/home/jb.lee/tmp/gemma-7b/flax-1/flax_model.msgpack" \
---load_gemma_config='7b' \
+--load_checkpoint="flax_params::$1" \
+--load_gemma_config='2b' \
 --input_length=30 \
 --seq_length=8192 \
 --add_bos_token 
